4be0164e46d11c3e3f5af194e9688a92c8fea8dd
Ayikanying-ux <allanayikanying72@gmail.com>
2022-09-20 12:05:49 -0700

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    print("{:d}".format(i).rjust(2, "0"),
-          end="\n" if i == 99 else ", ")
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
