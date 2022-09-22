5900cb5c20628bc513c85c9ed6234f6110c4e94e
Ayikanying-ux <allanayikanying72@gmail.com>
2022-09-22 10:47:46 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,11 @@
-#!#!/usr/bin/python3
-if __name__ == "main":
+#!/usr/bin/python3
+# 2-args.py
+
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
     import sys
+
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
